2440dda8f0be15c5f1fac859fb7462ec0f405c46
MarkTEM <82883602+MarkTEM@users.noreply.github.com>
2021-08-02 14:16:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function preloader() {
+    $(() => {
+        setInterval(() => {
+
+          let p = $('.preloader');
+
+          p.css('opacity', 0);
+
+          setInterval(
+            () => p.remove(),
+            parseInt(p.css('--duration')) *1000
+          );
+
+        }, 1000);
+
+    });
+}
+
+preloader();
+
+setInterval(() => preloader(), 5000);
