0555f9212aa1e2375c1ce820f5ef3b7fda1d1bc0
MarkTEM <82883602+MarkTEM@users.noreply.github.com>
2021-11-14 05:18:33 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -21,16 +21,6 @@
 // setInterval(() => preloader(), 0);
 
 
-let preloader = document.querySelector('.preloader');
-
-window.addEventListener('load', () => {
-  preloader.classList.add('hide');
-  setTimeout(() => {
-    preloader.remove()
-  },600)
-});
-
-
 
 if (!localStorage.theme) localStorage.theme = "light"
 document.body.className = localStorage.theme
@@ -42,3 +32,18 @@ DarkMode.onclick = () => {
     DarkMode.innerText = document.body.classList.contains("dark") ? "Светлая тема" : "Тёмная тема"
     localStorage.theme = document.body.className || "light"
 }
+
+
+
+document.querySelector('.b-7').addEventListener('click' , () => {
+  let data = document.querySelector('i-7').innerHTML = '';
+});
+
+
+
+
+
+
+
+
+
