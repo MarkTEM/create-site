47358caca4f22b61f10424049591878ea7a8f7b4
MarkTEM <82883602+MarkTEM@users.noreply.github.com>
2021-11-17 14:19:37 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,7 +20,14 @@
 
 // setInterval(() => preloader(), 0);
 
+let preloader = document.querySelector('.preloader');
 
+window.addEventListener('load', () => {
+  preloader.classList.add('hide');
+  setTimeout(() => {
+    preloader.remove()
+  },600)
+});
 
 if (!localStorage.theme) localStorage.theme = "light"
 document.body.className = localStorage.theme
